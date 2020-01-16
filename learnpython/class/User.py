a19e55ab8e14b23a3293cc505eaf4a3345ef781e
yuxuan su <497572121@qq.com>
2020-01-16 09:53:15 +0800

    练习2020.1.15

---
+++ learnpython/class/User.py
@@ -0,0 +1,16 @@
+class User():
+
+    def __init__(self, first_name, last_name, user_info):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_info = user_info
+
+    def describe_user(self):
+        print(self.user_info)
+
+    def greet_user(self):
+        print("\nHello " + self.first_name.title() + " " + self.last_name.title() + "! Welcome to here!")
+
+suyuxuan = User('yuxuan', 'su', 'haha')
+suyuxuan.describe_user()
+suyuxuan.greet_user()
