8878edc7e54610b2a9ecf988da8515292b93cd14
suyuxuan <497572121@qq.com>
2021-07-05 23:59:00 +0800

    2021.7.5

--- learnpython/chapter10/pi_string.py
+++ learnpython/chapter10/pi_string.py
@@ -3,7 +3,10 @@ filename = 'pi_digits.txt'
 with open(filename) as file_object:
     lines = file_object.readlines()
 
+print(lines)
+
 pi_string = ''
+# 创建了一个变量——pi_string，用于存储圆周率的值
 for line in lines:
     pi_string += line.strip()
 
--- learnpython/greeter.py
+++ learnpython/greeter.py
@@ -1,2 +1,6 @@
-name = input("Please enter your name: ")
-print("Hello! " + name + "!")
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat's your first name?"
+
+name = input(prompt)
+
+print("\nHello, " + name + "!")
