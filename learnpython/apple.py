328ac53409cbbc1dcc0d24f02e7c9de93813137e
suyuxuan <497572121@qq.com>
2022-10-16 17:24:25 +0800

    dddddddddddd

---
+++ learnpython/apple.py
@@ -0,0 +1,12 @@
+from cmd import PROMPT
+from unicodedata import name
+
+promot = "hello"
+
+while True:
+    name = input(promot)
+
+    if name == "quit!":
+        break
+    else:
+        print("The name " + name.title() + " you called was wrong!")
