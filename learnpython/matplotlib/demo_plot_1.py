1356b85c2b673925f1fc89ff45f54fb499d342d0
suyuxuan <497572121@qq.com>
2022-11-10 13:15:16 +0800

    practice

---
+++ learnpython/matplotlib/demo_plot_1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4],[10,20,25,30],color='lightblue',linewidth=2)
+plt.xlim(0.5,4.5)
+plt.ylim(5,100)
+plt.show()
---
+++ learnpython/matplotlib/demo_plot_2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+
